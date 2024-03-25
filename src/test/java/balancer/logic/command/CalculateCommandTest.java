b9babb729ab5411cec4531f9b4fbdb54cca59e91
LimZiJia <zijialim@gmail.com>
2024-03-26 02:07:36 +0800

    Fix test checkstyle

--- src/test/java/balancer/logic/command/CalculateCommandTest.java
+++ src/test/java/balancer/logic/command/CalculateCommandTest.java
@@ -1,51 +1,51 @@
 package balancer.logic.command;
 
-import balancer.storage.Transaction;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import balancer.storage.Transaction;
 
 class CalculateCommandTest {
-    CalculateCommand cc = new CalculateCommand();
+    private CalculateCommand cc = new CalculateCommand();
 
     @Test
-    public void one_person_pay_two_persons() {
+    public void onePersonPayTwoPersons() {
         HashMap<String, Transaction> transactions = new HashMap<>();
         transactions.put("Alice", new Transaction("Alice", 40));
         transactions.put("Bob", new Transaction("Bob", 40));
         transactions.put("Charlie", new Transaction("Charlie", 10));
         List<Transaction> transactionList = cc.preprocess(transactions);
 
-        String expected = "Charlie has to pay Alice $10.00\n" +
-                "Charlie has to pay Bob $10.00\n" +
-                "\n" +
-                "Number of transactions: 2";
+        String expected = "Charlie has to pay Alice $10.00\n"
+                + "Charlie has to pay Bob $10.00\n"
+                + "\n"
+                + "Number of transactions: 2";
         String actual = cc.greedy(transactionList);
         assertEquals(expected, actual);
     }
 
     @Test
-    public void one_person_pay_two_persons_2dp() {
+    public void onePersonPayTwoPersons2dp() {
         HashMap<String, Transaction> transactions = new HashMap<>();
         transactions.put("Alice", new Transaction("Alice", 40.105));
         transactions.put("Bob", new Transaction("Bob", 40.105));
         transactions.put("Charlie", new Transaction("Charlie", 10));
         List<Transaction> transactionList = cc.preprocess(transactions);
 
-        String expected = "Charlie has to pay Alice $10.04\n" +
-                "Charlie has to pay Bob $10.03\n" +
-                "\n" +
-                "Number of transactions: 2";
+        String expected = "Charlie has to pay Alice $10.04\n"
+                + "Charlie has to pay Bob $10.03\n"
+                + "\n"
+                + "Number of transactions: 2";
         String actual = cc.greedy(transactionList);
         assertEquals(expected, actual);
     }
 
     @Test
-    public void four_persons_1() {
+    public void fourPersons1() {
         HashMap<String, Transaction> transactions = new HashMap<>();
         transactions.put("Alice", new Transaction("Alice", 10));
         transactions.put("Bob", new Transaction("Bob", 20));
@@ -53,15 +53,15 @@ class CalculateCommandTest {
         transactions.put("Don", new Transaction("Don", 10));
         List<Transaction> transactionList = cc.preprocess(transactions);
 
-        String expected = "Charlie has to pay Bob $10.00\n" +
-                "\n" +
-                "Number of transactions: 1";
+        String expected = "Charlie has to pay Bob $10.00\n"
+                + "\n"
+                + "Number of transactions: 1";
         String actual = cc.greedy(transactionList);
         assertEquals(expected, actual);
     }
 
     @Test
-    public void four_persons_2() {
+    public void fourPersons2() {
         HashMap<String, Transaction> transactions = new HashMap<>();
         transactions.put("Alice", new Transaction("Alice", 40));
         transactions.put("Bob", new Transaction("Bob", 40));
@@ -69,16 +69,16 @@ class CalculateCommandTest {
         transactions.put("Don", new Transaction("Don", 10));
         List<Transaction> transactionList = cc.preprocess(transactions);
 
-        String expected = "Don has to pay Alice $15.00\n" +
-                "Charlie has to pay Bob $15.00\n" +
-                "\n" +
-                "Number of transactions: 2";
+        String expected = "Don has to pay Alice $15.00\n"
+                + "Charlie has to pay Bob $15.00\n"
+                + "\n"
+                + "Number of transactions: 2";
         String actual = cc.greedy(transactionList);
         assertEquals(expected, actual);
     }
 
     @Test
-    public void four_persons_with_one_significantly_more() {
+    public void fourPersonsWithOneSignificantlyMmore() {
         HashMap<String, Transaction> transactions = new HashMap<>();
         transactions.put("Alice", new Transaction("Alice", 200));
         transactions.put("Bob", new Transaction("Bob", 80));
@@ -86,17 +86,17 @@ class CalculateCommandTest {
         transactions.put("Don", new Transaction("Don", 20));
         List<Transaction> transactionList = cc.preprocess(transactions);
 
-        String expected = "Don has to pay Alice $67.50\n" +
-                "Charlie has to pay Alice $37.50\n" +
-                "Bob has to pay Alice $7.50\n" +
-                "\n" +
-                "Number of transactions: 3";
+        String expected = "Don has to pay Alice $67.50\n"
+                + "Charlie has to pay Alice $37.50\n"
+                + "Bob has to pay Alice $7.50\n"
+                + "\n"
+                + "Number of transactions: 3";
         String actual = cc.greedy(transactionList);
         assertEquals(expected, actual);
     }
 
     @Test
-    public void four_persons_with_two_significantly_more() {
+    public void fourPersonsWithTwoSignificantlyMore() {
         HashMap<String, Transaction> transactions = new HashMap<>();
         transactions.put("Alice", new Transaction("Alice", 160));
         transactions.put("Bob", new Transaction("Bob", 120));
@@ -104,15 +104,12 @@ class CalculateCommandTest {
         transactions.put("Don", new Transaction("Don", 20));
         List<Transaction> transactionList = cc.preprocess(transactions);
 
-        String expected = "Don has to pay Alice $67.50\n" +
-                "Charlie has to pay Bob $32.50\n" +
-                "Charlie has to pay Alice $5.00\n" +
-                "\n" +
-                "Number of transactions: 3";
+        String expected = "Don has to pay Alice $67.50\n"
+                + "Charlie has to pay Bob $32.50\n"
+                + "Charlie has to pay Alice $5.00\n"
+                + "\n"
+                + "Number of transactions: 3";
         String actual = cc.greedy(transactionList);
         assertEquals(expected, actual);
     }
-
-    
-
 }
